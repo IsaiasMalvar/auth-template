87b6f0fb7cb6e17708e342c301a9914ee868c393
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2024-04-21 17:49:13 +0100

    create variants of same component for client and server

--- auth.ts
+++ auth.ts
@@ -54,6 +54,10 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
       return session;
     },
     async jwt({ token }) {
@@ -64,6 +68,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
